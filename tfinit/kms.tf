8d3e77d6d108d731d1f3a0c0cbf079687c064d08
awsandy <awsandy@amazon.co.uk>
2023-04-11 15:50:19 +0100

    up

--- tfinit/kms.tf
+++ tfinit/kms.tf
@@ -18,6 +18,18 @@ resource "aws_kms_key_policy" "xgovkey" {
 
         Resource = "*"
         Sid      = "Enable IAM User Permissions"
+        Condition: {
+                ArnLike: {
+                  aws:PrincipalArn: [
+                        format("arn:aws:sts::%s:assumed-role/lf-admin/AWSLF-00-AT-xxxxxxxxxxxx-*",data.aws_caller_identity.current.account_id),
+                        format("arn:aws:iam::%s:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess",data.aws_caller_identity.current.account_id),
+                        format("arn:aws:iam::%s:role/EMRContainers-JobExecutionRole-at",data.aws_caller_identity.current.account_id),
+                        format("arn:aws:iam::%s:role/lf-admin",data.aws_caller_identity.current.account_id),
+                        format("arn:aws:sts::%s:assumed-role/lf-admin/Participant",data.aws_caller_identity.current.account_id)
+                    ]
+                }
+        }
+
       },
     ]
     Version = "2012-10-17"
