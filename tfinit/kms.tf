a01f9ad32e824c553300feee042a745302bb3fa7
awsandy <awsandy@amazon.co.uk>
2023-04-11 16:13:05 +0100

    up

--- tfinit/kms.tf
+++ tfinit/kms.tf
@@ -39,7 +39,6 @@ data "aws_iam_policy_document" "xgovkey" {
     resources = [
       "*",
     ]
-    principal = [ "*", ]
     condition {
       test     = "ArnLike"
       variable = "aws:PrincipalArn"
