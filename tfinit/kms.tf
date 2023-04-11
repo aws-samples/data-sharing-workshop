3d76d4a8959e75c6df0f60c95ac887e006bac153
awsandy <awsandy@amazon.co.uk>
2023-04-11 16:06:50 +0100

    up

--- tfinit/kms.tf
+++ tfinit/kms.tf
@@ -6,37 +6,37 @@ resource "aws_kms_key" "xgovkey" {
 
 resource "aws_kms_key_policy" "xgovkey" {
   key_id = aws_kms_key.xgovkey.id
-  policy = jsonencode({
-    Id = "xgovkey"
-    Statement = [
-      {
-        Action = "kms:*"
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        }
+  policy = data.aws_iam_policy_document.xgovkey.json
+}
 
-        Resource = "*"
-        Sid      = "Enable IAM User Permissions"
-        Condition = {
-                ArnLike = {
-                  aws:PrincipalArn = [
-                        format("arn:aws:sts::%s:assumed-role/lf-admin/AWSLF-00-AT-xxxxxxxxxxxx-*",data.aws_caller_identity.current.account_id),
-                        format("arn:aws:iam::%s:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess",data.aws_caller_identity.current.account_id),
-                        format("arn:aws:iam::%s:role/EMRContainers-JobExecutionRole-at",data.aws_caller_identity.current.account_id),
-                        format("arn:aws:iam::%s:role/lf-admin",data.aws_caller_identity.current.account_id),
-                        format("arn:aws:sts::%s:assumed-role/lf-admin/Participant",data.aws_caller_identity.current.account_id)
-                    ]
-                }
-        }
 
-      },
+data "aws_iam_policy_document" "xgovkey" {
+  statement {
+    actions = [
+      "kms:*",
     ]
-    Version = "2012-10-17"
-  })
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "ArnLike"
+      variable = "aws:PrincipalArn"
+      values = [
+        format("arn:aws:sts::%s:assumed-role/lf-admin/AWSLF-00-AT-xxxxxxxxxxxx-*",data.aws_caller_identity.current.account_id),
+        format("arn:aws:iam::%s:role/aws-service-role/lakeformation.amazonaws.com/AWSServiceRoleForLakeFormationDataAccess",data.aws_caller_identity.current.account_id),
+        format("arn:aws:iam::%s:role/EMRContainers-JobExecutionRole-at",data.aws_caller_identity.current.account_id),
+        format("arn:aws:iam::%s:role/lf-admin",data.aws_caller_identity.current.account_id),
+        format("arn:aws:sts::%s:assumed-role/lf-admin/Participant",data.aws_caller_identity.current.account_id),
+      ]
+    }
+  }
+
+
 }
 
 
 output "keyid" {
   value = aws_kms_key.xgovkey.key_id
 }
+
+
