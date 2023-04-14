ac90fdd24bdd43f8914eb0c24efac47d5192ec6e
awsandy <awsandy@amazon.co.uk>
2023-04-14 14:25:23 +0100

    cse job

--- .cse/05-job.sh
+++ .cse/05-job.sh
@@ -67,12 +67,12 @@ job=$(aws emr-containers start-job-run --cli-input-json file://cse-data.json)
 echo $job | jq .
 jid=$(echo $job | jq -r .id)
 if [[ $jid != "" ]]; then
-  echo " waiting for the job ~ 6 minutes"
+  echo "waiting for the job ~ 6 minutes"
   js=$(aws emr-containers describe-job-run --virtual-cluster-id $VIRTUAL_CLUSTER_ID --id $jid --query jobRun.state --output text)
   while [[ $js != "COMPLETED" ]] && [[ $js != "FAILED" ]]; do
-    echo "waiting for cloudformation deletion"
+    echo "waiting for spark job $jid on virtual EMR cluster $VIRTUAL_CLUSTER_ID current state $js"
     sleep 10
     js=$(aws emr-containers describe-job-run --virtual-cluster-id $VIRTUAL_CLUSTER_ID --id $jid --query jobRun.state --output text)
   done
-  echo $js
+  echo "End status of job = $js"
 fi
