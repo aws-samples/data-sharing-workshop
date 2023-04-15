0cea54b469e5b07962d3a6e4776c3540e945a611
awsandy <awsandy@amazon.co.uk>
2023-04-15 10:15:48 +0100

    cse perms

--- .cse/03-lf.sh
+++ .cse/03-lf.sh
@@ -23,7 +23,7 @@ EOF
 echo "Adding EMR Role to Lake Formation admins"
 aws lakeformation put-data-lake-settings --data-lake-settings file://lf-settings.json
 
-echo "Grant LF-GlueService-Role"
+echo "Grant EMRContainers Role ALL on xgov"
 cat <<EOF >input.json
 {
     "CatalogId": "${accid}",
@@ -45,7 +45,28 @@ EOF
 echo "Adding EMR Role with permissions ALL for database xgov"
 aws lakeformation grant-permissions --cli-input-json file://input.json
 
-
+echo "Grant EMRContainers Role ALL on customers table"
+cat <<EOF >input.json
+{
+    "CatalogId": "${accid}",
+    "Principal": {
+        "DataLakePrincipalIdentifier": "arn:aws:iam::${accid}:role/EMRContainers-JobExecutionRole-at"
+    },
+    "Resource": {
+        "TABLE": {
+            "CatalogId": "${accid}",
+            "DatabaseName" "xgov"
+            "Name": "customers${TF_VAR_team_number}"
+        }
+     },
+    "Permissions": [
+        "ALL"
+    ],
+    "PermissionsWithGrantOption": []
+}
+EOF
+echo "Adding EMR Role with permissions ALL for table customers"
+aws lakeformation grant-permissions --cli-input-json file://input.json
 
 
 
