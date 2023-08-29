bbf3cba0242ae528447018d9a197eae7c0bd72b3
awsandy <awsandy@amazon.co.uk>
2023-08-29 17:13:49 +0100

    up

--- .aws-staff/check-04-lf-perms.sh
+++ .aws-staff/check-04-lf-perms.sh
@@ -1,10 +1,38 @@
 echo "check you see 25 lf-admin permissions"
-aws lakeformation list-permissions --output text | grep lf-admin | wc -l
-aws lakeformation list-permissions --output text | grep lf-camp | wc -l
-aws lakeformation list-permissions --output text | grep lf-devel | wc -l
-echo " check there are 11 tag keys"
-aws lakeformation list-lf-tags --query LFTags[].TagKey | jq -r .[] | wc -l
-echo "check there are 6 public values"
-aws lakeformation list-permissions --output text | grep public | wc -l
-echo "check customersX is tagged to share with teams - should see output customersX"
-aws lakeformation search-tables-by-lf-tags --expression TagKey=share,TagValues=teams --query TableList[].Table[].Name --output text
+ac=$(aws lakeformation list-permissions --output text | grep lf-admin | wc -l)
+if [[ $ac -lt 25 ]];then
+    echo "ERROR: expected 25+ lf-admin permissions only got $sc"
+else
+    echo "PASSED: Found expected 25+ lf-admin permissions"
+fi
+cc=$(aws lakeformation list-permissions --output text | grep lf-campaign-manager | wc -l)
+if [[ $cc -lt 2 ]];then
+    echo "ERROR: expected 2 lf-campaign-manager permissions got $cc"
+else
+    echo "PASSED: Found expected 2 lf-campaign-manager permissions"
+fi
+dc=$(aws lakeformation list-permissions --output text | grep lf-developer | wc -l)
+if [[ $dc -lt 2 ]];then
+    echo "ERROR: expected 2 lf-developer permissions got $dc"
+else
+    echo "PASSED: Found expected 2 lf-developer permissions"
+fi
+tk=$(aws lakeformation list-lf-tags --query LFTags[].TagKey | jq -r .[] | wc -l)
+if [[ $tk -ne 11 ]];then
+    echo "ERROR: expected 11 tag keys got $tk"
+else
+    echo "PASSED: Found expected 11 tag keys"
+fi
+pv=$(aws lakeformation list-permissions --output text | grep public | wc -l)
+if [[ $pv -ne 11 ]];then
+    echo "ERROR: expected 6 public values got $pv"
+else
+    echo "PASSED: Found expected 6 public values"
+fi
+aws lakeformation search-tables-by-lf-tags --expression TagKey=share,TagValues=teams --query TableList[].Table[].Name --output text | grep customers > /dev/null
+if [[ $ip -eq 0 ]]; then
+    echo "PASSED: customers table found in share tag"
+
+else
+    echo "ERROR: customers table not found in share tag"
+fi
