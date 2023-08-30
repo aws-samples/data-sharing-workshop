a015df786f5d49de2fffdad611051a81fa71cfc3
awsandy <awsandy@amazon.co.uk>
2023-08-30 16:19:19 +0100

    up

--- .aws-staff/check-05-ram-shares.sh
+++ .aws-staff/check-05-ram-shares.sh
@@ -1,2 +1,7 @@
 echo 'check there are "4" shares'
-aws ram get-resource-shares --resource-owner SELF --query resourceShares[].name | jq -r .[] | grep LakeF | wc -l
+rs=$(aws ram get-resource-shares --resource-owner SELF --query resourceShares[].name | jq -r .[] | grep LakeF | wc -l)
+if [[ $rs -ne 4 ]];then
+    echo "ERROR: Expected to see 4 RAM shares - got $rs"
+else
+    echo "PASSED: Found 4 RAM shares as expected"
+fi
