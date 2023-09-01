f9a9417bf8b5181f8d87d007cf43e789f3e823c7
awsandy <awsandy@amazon.co.uk>
2023-09-01 10:35:10 +0100

    up

--- .aws-staff/check-06-xacct.sh
+++ .aws-staff/check-06-xacct.sh
@@ -1,5 +1,45 @@
+if [[ -z ${TF_VAR_team_number+x} ]]; then
+    echo "TF_VAR_team_number not set"
+    read -p 'Enter your Team Number 1,2 or 3: ' tn
+    if [[ $tn =~ ^[1-3]{1}$ ]]; then
+        export TF_VAR_team_number=${tn}
+        echo "export TF_VAR_team_number=${tn}" | tee -a ~/.bash_profile
+    else
+        echo "Please enter a team number between 1 and 3"
+        exit
+    fi
+    
+
+fi
+if [[ -z ${TF_VAR_remote_acct_1+x} ]]; then
+    echo "TF_VAR_remote_acct_1 not set"
+    read -p 'Enter Remote Account ID 1: ' a1
+    if [[ $a1 =~ ^[0-9]{12}$ ]]; then
+        echo "Account 1 = $a1"
+        export TF_VAR_remote_acct_1=${a1}
+        echo "export TF_VAR_remote_acct_1=${a1}" | tee -a ~/.bash_profile
+    else
+        echo "Please enter a 12 digit AWS account number"
+        exit
+    fi
+    
+fi
+if [[ -z ${TF_VAR_remote_acct_2+x} ]]; then
+    echo "TF_VAR_remote_acct_2 not set"
+    read -p 'Enter Remote Account ID 2: ' a2
+    if [[ $a2 =~ ^[0-9]{12}$ ]]; then
+        echo "Account 2 = $a2"
+        export TF_VAR_remote_acct_2=${a2}
+        echo "export TF_VAR_remote_acct_2=${a2}" | tee -a ~/.bash_profile
+        
+    else
+        echo "Please enter a 12 digit AWS account number"
+        exit
+    fi
+    
+fi
 ac=$(aws sts get-caller-identity | jq -r .Account)
-cracs=$(aws lakeformation list-permissions | grep 'iam:'| grep -v $ac | sort -u | cut -f6 -d: | wc -l)
+cracs=$(aws lakeformation list-permissions | grep 'iam:'| grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f6 -d: | wc -l)
 if [[ $cracs -ne 2 ]];then
     echo "WARNING: found $cracs remote accounts in permissions - expected only 2 at this point in the workshop"
 fi
@@ -18,7 +58,7 @@ else
     echo "PASSED: Found min 4 principals expected"
 fi
 
-racs=$(aws lakeformation list-permissions | grep 'iam:'| grep -v $ac | sort -u | cut -f6 -d:)
+racs=$(aws lakeformation list-permissions | grep 'iam:'| grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f6 -d:)
 
 for ra in $racs; do
 cat << EOF > input.json
@@ -75,7 +115,7 @@ fi
 
 done
 
-prins=$(aws lakeformation list-permissions | grep 'iam:' | grep -v $ac | sort -u | cut -f2- -d':' | jq -r .)
+prins=$(aws lakeformation list-permissions | grep 'iam:' | grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f2- -d':' | jq -r .)
 # check db perms
 for p in $prins; do
 
