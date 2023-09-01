9ea49a0d819de7ab92357046720d3a6c6d64366f
awsandy <awsandy@amazon.co.uk>
2023-09-01 10:38:01 +0100

    up

--- .aws-staff/check-06-xacct.sh
+++ .aws-staff/check-06-xacct.sh
@@ -8,7 +8,6 @@ if [[ -z ${TF_VAR_team_number+x} ]]; then
         echo "Please enter a team number between 1 and 3"
         exit
     fi
-    
 
 fi
 if [[ -z ${TF_VAR_remote_acct_1+x} ]]; then
@@ -22,7 +21,7 @@ if [[ -z ${TF_VAR_remote_acct_1+x} ]]; then
         echo "Please enter a 12 digit AWS account number"
         exit
     fi
-    
+
 fi
 if [[ -z ${TF_VAR_remote_acct_2+x} ]]; then
     echo "TF_VAR_remote_acct_2 not set"
@@ -31,37 +30,39 @@ if [[ -z ${TF_VAR_remote_acct_2+x} ]]; then
         echo "Account 2 = $a2"
         export TF_VAR_remote_acct_2=${a2}
         echo "export TF_VAR_remote_acct_2=${a2}" | tee -a ~/.bash_profile
-        
+
     else
         echo "Please enter a 12 digit AWS account number"
         exit
     fi
-    
+
 fi
 ac=$(aws sts get-caller-identity | jq -r .Account)
-cracs=$(aws lakeformation list-permissions | grep 'iam:'| grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f6 -d: | wc -l)
-if [[ $cracs -ne 2 ]];then
+cracs=$(aws lakeformation list-permissions | grep 'iam:' | grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f6 -d: | wc -l)
+if [[ $cracs -ne 2 ]]; then
     echo "WARNING: found $cracs remote accounts in permissions - expected only 2 at this point in the workshop"
 fi
 
 rs=$(aws ram get-resource-shares --resource-owner SELF --query resourceShares[].name | jq -r .[] | grep LakeF | wc -l)
-if [[ $rs -ne 4 ]];then
+if [[ $rs -lt 4 ]]; then
     echo "ERROR: Expected to see 4 RAM shares - got $rs"
-else
+elif [[ $rs -eq 4 ]]; then
     echo "PASSED: Found 4 RAM shares as expected"
+else
+    echo "ERROR: Expected to see 4 principlas in LF permissions - got $xc"
 fi
 ac=$(aws sts get-caller-identity | jq -r .Account)
 xc=$(aws lakeformation list-permissions | grep 'iam:' | grep -v $ac | wc -l)
-if [[ $rs -lt 4 ]];then
+if [[ $rs -lt 4 ]]; then
     echo "ERROR: Expected to see 4 principlas in LF permissions - got $xc"
 else
     echo "PASSED: Found min 4 principals expected"
 fi
 
-racs=$(aws lakeformation list-permissions | grep 'iam:'| grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f6 -d:)
+racs=$(aws lakeformation list-permissions | grep 'iam:' | grep -e $TF_VAR_remote_acct_1 -e $TF_VAR_remote_acct_2 | sort -u | cut -f6 -d:)
 
 for ra in $racs; do
-cat << EOF > input.json
+    cat <<EOF >input.json
 {
     "CatalogId": "$ac",
     "Resource": {
@@ -73,21 +74,21 @@ cat << EOF > input.json
     }
 }
 EOF
-perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$ra)
-echo $perms | grep DESCRIBE > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $ra has DESCRIBE on tag share value teams"
-else
-echo "ERROR: Principal $ra does not have DESCRIBE on tag share value teams"
-fi
-echo $perms | grep ASSOCIATE  > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $ra has ASSOCIATE on tag share value teams"
-else
-echo "ERROR: Principal $ra does not have ASSOCIATE on tag share value teams"
-fi
+    perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$ra)
+    echo $perms | grep DESCRIBE >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $ra has DESCRIBE on tag share value teams"
+    else
+        echo "ERROR: Principal $ra does not have DESCRIBE on tag share value teams"
+    fi
+    echo $perms | grep ASSOCIATE >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $ra has ASSOCIATE on tag share value teams"
+    else
+        echo "ERROR: Principal $ra does not have ASSOCIATE on tag share value teams"
+    fi
 
-cat << EOF > input.json
+    cat <<EOF >input.json
 {
     "CatalogId": "$ac",
     "Resource": {
@@ -99,19 +100,19 @@ cat << EOF > input.json
     }
 }
 EOF
-perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$ra)
-echo $perms | grep DESCRIBE > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $ra has DESCRIBE on tag sensitivity value public"
-else
-echo "ERROR: Principal $ra does not have DESCRIBE on tag tag sensitivity value public"
-fi
-echo $perms | grep ASSOCIATE  > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $ra has ASSOCIATE on tag tag sensitivity value public"
-else
-echo "ERROR: Principal $ra does not have ASSOCIATE on tag tag sensitivity value public"
-fi
+    perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$ra)
+    echo $perms | grep DESCRIBE >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $ra has DESCRIBE on tag sensitivity value public"
+    else
+        echo "ERROR: Principal $ra does not have DESCRIBE on tag tag sensitivity value public"
+    fi
+    echo $perms | grep ASSOCIATE >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $ra has ASSOCIATE on tag tag sensitivity value public"
+    else
+        echo "ERROR: Principal $ra does not have ASSOCIATE on tag tag sensitivity value public"
+    fi
 
 done
 
@@ -119,7 +120,7 @@ prins=$(aws lakeformation list-permissions | grep 'iam:' | grep -e $TF_VAR_remot
 # check db perms
 for p in $prins; do
 
-cat << EOF > input.json
+    cat <<EOF >input.json
 {
     "CatalogId": "$ac",
     "Resource": {
@@ -138,21 +139,20 @@ cat << EOF > input.json
     }
 }
 EOF
-perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$p)
-echo $perms | grep DESCRIBE > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $p has DESCRIBE on DATABASE xgov"
-else
-echo "ERROR: Principal $p does not have DESCRIBE on DATABASE xgov"
-fi
+    perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$p)
+    echo $perms | grep DESCRIBE >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $p has DESCRIBE on DATABASE xgov"
+    else
+        echo "ERROR: Principal $p does not have DESCRIBE on DATABASE xgov"
+    fi
 done
-
 
 # check table perms
 
 for p in $prins; do
 
-cat << EOF > input.json
+    cat <<EOF >input.json
 {
     "CatalogId": "$ac",
     "Resource": {
@@ -178,19 +178,18 @@ cat << EOF > input.json
     }
 }
 EOF
-perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$p)
-echo $perms | grep SELECT > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $p has SELECT with tags sensitivity: public,private and share: teams TABLE in xgov"
-else
-echo "ERROR: Principal $p does not have SELECT with tags sensitivity: public,private and share: teams TABLE in xgov"
-fi
-echo $perms | grep DESCRIBE > /dev/null
-if [[ $? -eq 0 ]];then
-echo "PASSED: Principal $p has DESCRIBE with tags sensitivity: public,private and share: teams TABLE in xgov"
-else
-echo "ERROR: Principal $p does not have DESCRIBE with tags sensitivity: public,private and share: teams TABLE in xgov"
-fi
-
+    perms=$(aws lakeformation list-permissions --cli-input-json file://input.json --principal DataLakePrincipalIdentifier=$p)
+    echo $perms | grep SELECT >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $p has SELECT with tags sensitivity: public,private and share: teams TABLE in xgov"
+    else
+        echo "ERROR: Principal $p does not have SELECT with tags sensitivity: public,private and share: teams TABLE in xgov"
+    fi
+    echo $perms | grep DESCRIBE >/dev/null
+    if [[ $? -eq 0 ]]; then
+        echo "PASSED: Principal $p has DESCRIBE with tags sensitivity: public,private and share: teams TABLE in xgov"
+    else
+        echo "ERROR: Principal $p does not have DESCRIBE with tags sensitivity: public,private and share: teams TABLE in xgov"
+    fi
 
 done
