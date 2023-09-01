5871d6dca7431d491a0cc6270346bf138750e2fb
awsandy <awsandy@amazon.co.uk>
2023-09-01 10:07:02 +0100

    up

--- .aws-staff/check-04-lf-perms.sh
+++ .aws-staff/check-04-lf-perms.sh
@@ -35,3 +35,10 @@ if [[ $ip -eq 0 ]]; then
 else
     echo "ERROR: customers table not found in share tag"
 fi
+echo 'check there are "4" shares'
+rs=$(aws ram get-resource-shares --resource-owner SELF --query resourceShares[].name | jq -r .[] | grep LakeF | wc -l)
+if [[ $rs -ne 4 ]];then
+    echo "ERROR: Expected to see 4 RAM shares - got $rs"
+else
+    echo "PASSED: Found 4 RAM shares as expected"
+fi
